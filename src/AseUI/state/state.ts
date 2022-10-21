27a9b7a8c99db8a8019731add4454152efe29994
JuanDAC <juanavila12.0308@gmail.com>
2022-10-20 23:47:48 -0500

    [🏷️] (generic types)

--- src/AseUI/state/state.ts
+++ src/AseUI/state/state.ts
@@ -1,56 +1,59 @@
 import type { AseView } from '../window';
 
-type InitialShareProps = { group: string; ids: string[]; key: string; initialValue: any; modify?: boolean };
+type ValueState = string | boolean | number | unknown;
 
-type UpdateShareProps = { group: string; key: string; update: (this: any, value: any) => any };
+type InitialShareProps<T = ValueState> = { group: string; ids: string[]; key: string; initialValue: T; modify?: boolean };
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type UpdateShareProps<T = ValueState> = { group: string; key: string; update: (this: any, value: T) => T; rebuild?: boolean };
 
 type ObtainShareProps = { group: string; key: string };
 
-type InitialProps = { id: string; key: string; initialValue: any; modify?: boolean };
+type InitialProps<T = ValueState> = { id: string; key: string; initialValue: T; modify?: boolean };
 
-type UpdateProps = { id: string; key: string; update: (this: any, value: any) => any };
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type UpdateProps<T = ValueState> = { id: string; key: string; update: (this: any, value: T) => T; rebuild?: boolean };
 
 type ObtainProps = { id: string; key: string };
 
-export class State extends Map<string, { value: any; modify: boolean; ids?: string[] }> {
+export class State extends Map<string, { value: ValueState; modify: boolean; ids?: string[] }> {
   private _view: AseView;
   constructor(view: AseView) {
     super();
     this._view = view;
   }
 
-  initialShare({ group, ids, key, initialValue, modify = true }: InitialShareProps): void {
+  initialShare<T = ValueState>({ group, ids, key, initialValue, modify = true }: InitialShareProps<T>): void {
     this.set(`group/${group}/${key}`, { value: initialValue, ids, modify });
   }
 
-  updateShare({ group, key, update }: UpdateShareProps): void {
-    const { value: oldValue, ids, modify } = this.get(`group/${group}/${key}`) as { ids: string[]; value: any; modify: boolean };
-    const value = update(oldValue);
-    this.set(`group/${group}/${key}`, { value, modify, ids });
-    if (modify) ids.forEach((id) => this._view.modify(id, key, value));
-    if (!modify) this._view.update();
+  updateShare<T = ValueState>({ group, key, update, rebuild = true }: UpdateShareProps<T>): void {
+    const { value: oldValue, ids, modify } = this.get(`group/${group}/${key}`) ?? {};
+    const value = update(oldValue as T);
+    this.set(`group/${group}/${key}`, { value, modify: !!modify, ids });
+    if (modify && rebuild) (ids ?? []).forEach((id) => this._view.modify(id, key, value));
+    if (!modify && rebuild) this._view.update();
   }
 
-  obtainShare({ group, key }: ObtainShareProps): any {
-    const { value } = this.get(`group/${group}/${key}`) as { ids: string[]; value: any };
-    return value;
+  obtainShare<T = ValueState>({ group, key }: ObtainShareProps): T {
+    const { value } = this.get(`group/${group}/${key}`) ?? {};
+    return value as T;
   }
 
-  update({ id, key, update }: UpdateProps): void {
-    // TODO: Fix get value and storage variables
+  update<T = ValueState>({ id, key, update, rebuild = true }: UpdateProps<T>): void {
     const { value: oldValue, modify } = this.get(`${id}/${key}`) ?? {};
-    const value = update(oldValue);
+    const value = update(oldValue as T);
     this.set(`${id}/${key}`, { value, modify: !!modify });
-    if (modify) this._view.modify(id, key, value);
-    if (!modify) this._view.update();
+    if (modify && rebuild) this._view.modify(id, key, value);
+    if (!modify && rebuild) this._view.update();
   }
 
-  obtain({ id, key }: ObtainProps): any {
+  obtain<T = ValueState>({ id, key }: ObtainProps): T {
     const { value } = this.get(`${id}/${key}`) ?? {};
-    return value;
+    return value as T;
   }
 
-  initial({ id, key, initialValue, modify = true }: InitialProps): void {
+  initial<T = ValueState>({ id, key, initialValue, modify = true }: InitialProps<T>): void {
     this.set(`${id}/${key}`, { value: initialValue, modify });
   }
 }
