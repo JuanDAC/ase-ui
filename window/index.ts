5527df57eca4b7e565b2dc8f37519fcfea4c4908
JuanDAC <juanavila12.0308@gmail.com>
2022-10-30 11:44:55 -0500

    [🚚] (move the files to importing library)

--- window/index.ts
+++ window/index.ts
@@ -1,5 +1,6 @@
 import { AseComponent } from '../src/AseUI/window/aseComponent';
 import { AseView } from '../src/AseUI/window/aseView';
+import type { AseComponentMethodsProps, Component } from '../src/AseUI/window/interface';
 import { AseWindow } from '../src/AseUI/window/aseWindow';
 
-export { AseWindow, AseComponent, AseView };
+export { AseWindow, AseComponent, AseView, AseComponentMethodsProps, Component };
